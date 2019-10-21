52c85934a6c8167010d40b870b5e2f2846e65ddf
Francisco Sandoval <frasandova@gmail.com>
2019-10-20 22:55:37 -0300

    Usuario y token listo

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 // Requires
 var express = require('express');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
 //Inicializar variables
 var app = express();
 
+//Body Parser middleware function
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+
+// Importar Rutas
+var appRoutes = require('./routes/app');
+var usuarioRoutes = require('./routes/usuario');
+var loginRoutes = require('./routes/login');
 // Conexión a la base de datos
 mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB',(err, res)=>{
     if(err) throw err;
@@ -13,12 +24,16 @@ mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB',(err, res)=>{
 });
 
 //Rutas
-app.get('/',(req, res, next)=>{
-    res.status(403).json({
-        ok:true,
-        mensaje:'Peticion realizada correctamente'
-    });
-});
+// app.get('/',(req, res, next)=>{
+//     res.status(403).json({
+//         ok:true,
+//         mensaje:'Peticion realizada correctamente'
+//     });
+// });
+
+app.use('/usuario', usuarioRoutes);
+app.use('/login', loginRoutes);
+app.use('/', appRoutes);
 
 
 // Escuchar Peticiones
