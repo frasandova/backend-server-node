2918862775c89fcc1ebc97f7a0916e10c0c1406b
Francisco Sandoval <frasandova@gmail.com>
2019-11-02 23:56:21 -0300

    Crud Apirest y subida de imagenes finalizado

--- app.js
+++ app.js
@@ -19,6 +19,8 @@ var loginRoutes = require('./routes/login');
 var hospitalRoutes = require('./routes/hospital');
 var medicoRoutes = require('./routes/medico');
 var busquedaRoutes = require('./routes/busqueda');
+var uploadRoutes = require('./routes/upload');
+var imagenesRoutes = require('./routes/imagenes');
 
 // Conexión a la base de datos
 mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB',(err, res)=>{
@@ -27,19 +29,22 @@ mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB',(err, res)=>{
     console.log('Base de datos: \x1b[36m%s\x1b[0m', 'online');
 });
 
+// Server index config
+// var serveIndex = require('serve-index');
+// app.use(express.static(__dirname + '/'))
+// app.use('/uploads', serveIndex(__dirname + '/uploads'));
+
+
 //Rutas
-// app.get('/',(req, res, next)=>{
-//     res.status(403).json({
-//         ok:true,
-//         mensaje:'Peticion realizada correctamente'
-//     });
-// });
 
 app.use('/usuario', usuarioRoutes);
 app.use('/hospital', hospitalRoutes);
 app.use('/medico', medicoRoutes);
 app.use('/login', loginRoutes);
 app.use('/busqueda', busquedaRoutes);
+app.use('/upload', uploadRoutes);
+app.use('/img', imagenesRoutes);
+
 app.use('/', appRoutes);
 
 
